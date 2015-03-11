2420948fa8c550c24eac3af17a0cb5b8b0ee44ab
Camille Hodoul <camille.hodoul@gmail.com>
2015-03-11 13:04:33 +0100

    Doc, build a .min file
    Put a minified version in the build/ directory, put yuidoc in doc/

---
+++ doc/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Animation",
+        "Tween",
+        "animatePaper",
+        "easing",
+        "frameManager"
+    ],
+    "modules": [
+        "animatePaper"
+    ],
+    "allModules": [
+        {
+            "displayName": "animatePaper",
+            "name": "animatePaper",
+            "description": "An animation library for paper.js\ncamille.hodoul@gmail.com"
+        }
+    ]
+} };
+});
