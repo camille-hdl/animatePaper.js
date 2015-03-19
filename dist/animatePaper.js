22e1ba8c66b072a8e8abc5d60ee34b09e15f73d5
Camille Hodoul <camille.hodoul@gmail.com>
2015-03-19 22:36:05 +0100

    gulp config

--- dist/animatePaper.js
+++ dist/animatePaper.js
@@ -193,50 +193,6 @@
             }
         }
     };
-    var easing = {
-        linear: function(p) {
-            return p;
-        },
-        swing: function(p) {
-            return .5 - Math.cos(p * Math.PI) / 2;
-        },
-        Sine: function(p) {
-            return 1 - Math.cos(p * Math.PI / 2);
-        },
-        Circ: function(p) {
-            return 1 - Math.sqrt(1 - p * p);
-        },
-        Elastic: function(p) {
-            return p === 0 || p === 1 ? p : -Math.pow(2, 8 * (p - 1)) * Math.sin(((p - 1) * 80 - 7.5) * Math.PI / 15);
-        },
-        Back: function(p) {
-            return p * p * (3 * p - 2);
-        },
-        Bounce: function(p) {
-            var pow2, bounce = 4;
-            while (p < ((pow2 = Math.pow(2, --bounce)) - 1) / 11) {}
-            return 1 / Math.pow(4, 3 - bounce) - 7.5625 * Math.pow((pow2 * 3 - 2) / 22 - p, 2);
-        }
-    };
-    var __tempEasing = [ "Quad", "Cubic", "Quart", "Quint", "Expo" ];
-    for (var i = 0, l = __tempEasing.length; i < l; i++) {
-        easing[__tempEasing[i]] = function(p) {
-            return Math.pow(p, i + 2);
-        };
-    }
-    __tempEasing = null;
-    for (var name in easing) {
-        if (easing.hasOwnProperty(name)) {
-            var easeIn = easing[name];
-            easing["easeIn" + name] = easeIn;
-            easing["easeOut" + name] = function(p) {
-                return 1 - easeIn(1 - p);
-            };
-            easing["easeInOut" + name] = function(p) {
-                return p < .5 ? easeIn(p * 2) / 2 : 1 - easeIn(p * -2 + 2) / 2;
-            };
-        }
-    }
     function _pointDiff(a, b, operator) {
         if ([ "+", "-" ].indexOf(operator) === -1) return;
         if (typeof a === "undefined" || typeof b === "undefined") return;
@@ -471,6 +427,50 @@
     for (var i = 0, l = _colorProperties.length; i < l; i++) {
         _tweenPropHooks[_colorProperties[i] + "Color"] = _tweenPropHooks.Color;
     }
+    var easing = {
+        linear: function(p) {
+            return p;
+        },
+        swing: function(p) {
+            return .5 - Math.cos(p * Math.PI) / 2;
+        },
+        Sine: function(p) {
+            return 1 - Math.cos(p * Math.PI / 2);
+        },
+        Circ: function(p) {
+            return 1 - Math.sqrt(1 - p * p);
+        },
+        Elastic: function(p) {
+            return p === 0 || p === 1 ? p : -Math.pow(2, 8 * (p - 1)) * Math.sin(((p - 1) * 80 - 7.5) * Math.PI / 15);
+        },
+        Back: function(p) {
+            return p * p * (3 * p - 2);
+        },
+        Bounce: function(p) {
+            var pow2, bounce = 4;
+            while (p < ((pow2 = Math.pow(2, --bounce)) - 1) / 11) {}
+            return 1 / Math.pow(4, 3 - bounce) - 7.5625 * Math.pow((pow2 * 3 - 2) / 22 - p, 2);
+        }
+    };
+    var __tempEasing = [ "Quad", "Cubic", "Quart", "Quint", "Expo" ];
+    for (var i = 0, l = __tempEasing.length; i < l; i++) {
+        easing[__tempEasing[i]] = function(p) {
+            return Math.pow(p, i + 2);
+        };
+    }
+    __tempEasing = null;
+    for (var name in easing) {
+        if (easing.hasOwnProperty(name)) {
+            var easeIn = easing[name];
+            easing["easeIn" + name] = easeIn;
+            easing["easeOut" + name] = function(p) {
+                return 1 - easeIn(1 - p);
+            };
+            easing["easeInOut" + name] = function(p) {
+                return p < .5 ? easeIn(p * 2) / 2 : 1 - easeIn(p * -2 + 2) / 2;
+            };
+        }
+    }
     animatePaper.fx = {
         grow: function(path, settings) {
             animatePaper.animate(path, {
@@ -659,4 +659,3 @@
 })({}, function() {
     return this;
 }());
-//# sourceMappingURL=/Users/camille/github/animatePaper.js/build/../dist/animatePaper.map
