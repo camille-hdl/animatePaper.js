e7071a70619b5a5a1a52f6ea0ecda90305f5fe21
Camille Hodoul <chodoul@1egal2.com>
2017-04-22 18:12:58 +0200

    change build pipeline for typescript

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,15 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var streamify = require('gulp-streamify');
-var babelify = require('babelify');
+// var babelify = require('babelify');
+var tsify = require("tsify");
 
 var MODULE_NOM = "paper-animate";
 
 var exports = {};
 var sourceDir = "src/";
 var distDir = "dist";
-var babelPresets = ["es2015"];
+// var babelPresets = ["es2015"];
 
 // used for uglify, order matters
 var files = [
@@ -45,7 +46,7 @@ gulp.task('build-' + MODULE_NOM + '-dev', function () {
   var b = browserify({
     entries: sourceDir + "export.js",
     debug: false
-  }).transform(babelify, {presets: babelPresets});
+  });
 
   return b.bundle()
     .pipe(source('' + MODULE_NOM + '.min.js'))
@@ -62,7 +63,7 @@ gulp.task('build-' + MODULE_NOM + '', function () {
   var b = browserify({
     entries: sourceDir + "export.js",
     debug: true
-  }).transform(babelify, {presets: babelPresets});
+  });
 
   return b.bundle()
     .pipe(source('' + MODULE_NOM + '.min.js'))
@@ -76,11 +77,12 @@ gulp.task('build-' + MODULE_NOM + '-browser', function () {
   process.env.ANIMPAPER_STDLONE = true;
   // set up the browserify instance on a task basis
   var b = browserify({
-    entries: sourceDir + "export-standalone.js",
-    debug: true
-  }).ignore("paper").transform(babelify, {presets: babelPresets});
+    entries: sourceDir + "export-standalone.ts",
+    debug: false
+  }).ignore("paper").plugin("tsify", { noImplicitAny: false });
 
   return b.bundle()
+    .on('error', function (error) { console.error(error.toString()); })
     .pipe(source('' + MODULE_NOM + '-browser.min.js'))
     .pipe(buffer())
         .pipe(uglify())
